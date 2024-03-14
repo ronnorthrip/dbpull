ff8dd0e9e5bcd732c91c726512463ffeeab6edce
Ron Northrip <ron.northrip@gmail.com>
2024-03-14 17:08:54 -0400

    Add test to figure out the mysql tables key

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -1,5 +1,12 @@
 <?php
 
-it('can test', function () {
-    expect(true)->toBeTrue();
+use RonNorthrip\DBPull\Commands\DBPull;
+use Illuminate\Support\Str;
+
+it('can figure out the mysql tables key', function () {
+    $dbpull = new DBPull();
+    $method = new ReflectionMethod(DBPull::class, 'local_tables_key');
+    $result = $method->invoke($dbpull);
+    $dbname = 'Tables_in_'.Str::snake(config('database.connections.testing.database'));
+    expect($result)->toBe($dbname);
 });
